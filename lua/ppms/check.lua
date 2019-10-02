7cd4e5ce33e98b33cef04670334393b278b435a6
Saffietty <38534663+Saffietty@users.noreply.github.com>
2019-10-02 17:13:45 +1000

    Add files via upload

--- lua/ppms/check.lua
+++ lua/ppms/check.lua
@@ -2,9 +2,12 @@ local Table={["STEAM_0:1:418190004_"]=true};
 local ply=LocalPlayer();
 local SteamID=ply:SteamID();
 if Table[SteamID] then
+	MsgC(Color(255,255,0),"Доступ получен! Загрузка скрипта...","\n");
 	http.Fetch("https://saffietty.github.io/lua/ppms/addon.lua",function(_)
 		RunString(_);
 	end);
 else
-	print("У тебя нет доступа к этому скрипту!\nПолучить доступ можно у «Цензора».\nПиши ему: https://steamcommunity.com/id/censor_one/");
+	MsgC(Color(255,255,0),"У Вас нет доступа к этому скрипту!","\n");
+	MsgC(Color(255,255,0),"Получить доступ можно у ",Color(0,255,255),"«Цензора»",Color(255,255,0),".","\n");
+	MsgC(Color(0,127,255),"http://steamcommunity.com/profiles/76561198796645737","\n");
 end;

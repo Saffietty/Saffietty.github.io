2c073d17706c080b03239cea24f81d75f61bb7c0
Saffietty <38534663+Saffietty@users.noreply.github.com>
2019-10-02 18:27:37 +1000

    Add files via upload

--- lua/ppms/addon.lua
+++ lua/ppms/addon.lua
@@ -1,5 +1,5 @@
-MsgC(Color(255,255,0),"Скрипт запущен.","\n");
 local Addon=function()
+	MsgC(Color(255,255,0),"Скрипт запущен.","\n");
 	print("LOL, it's FAKE! :D");
 end;
 Addon();
--- lua/ppms/check.lua
+++ lua/ppms/check.lua
@@ -1,18 +1,18 @@
 local Check=function()
-	local Table={["STEAM_0:1:418190004"]=true};
+	local Table={["STEAM_0:1:418190004_"]=true};
 	local ply=LocalPlayer();
 	local SteamID=ply:SteamID();
-	if Table[SteamID] then
-		MsgC(Color(255,255,0),"Доступ получен! Загрузка скрипта...","\n");
+	if !Table[SteamID] then
+		MsgC(Color(255,255,0),"У Вас ",Color(255,127,0),"нет доступа",Color(255,255,0)," к этому скрипту!","\n");
+		MsgC(Color(255,255,0),"Получить доступ можно у ",Color(0,255,255),"«Цензора»",Color(255,255,0),".","\n");
+		MsgC(Color(0,127,255),"http://steamcommunity.com/profiles/76561198796645737","\n");
+	else
+		MsgC(Color(255,255,0),"Доступ получен, загрузка скрипта...","\n");
 		http.Fetch("https://saffietty.github.io/lua/ppms/addon.lua",function(body,_,_,code)
 			if code==200 then
 				RunString(body);
 			end;
 		end);
-	else
-		MsgC(Color(255,255,0),"У Вас нет доступа к этому скрипту!","\n");
-		MsgC(Color(255,255,0),"Получить доступ можно у ",Color(0,255,255),"«Цензора»",Color(255,255,0),".","\n");
-		MsgC(Color(0,127,255),"http://steamcommunity.com/profiles/76561198796645737","\n");
 	end;
 end;
 Check();
